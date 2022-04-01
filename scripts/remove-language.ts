18b54e2b3b105c76b1213521719f4a27f4244ad1
bmaupin <bmaupin@users.noreply.github.com>
2022-04-01 13:11:19 -0400

    feat: Add script to remove a language
    This is one I'd written a while back and never committed... It may need some testing if I ever use it

---
+++ scripts/remove-language.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+import { readFile, writeFile } from 'fs/promises';
+import { Language, Score } from './classes/DataPopulator';
+
+const main = async () => {
+  console.log('process.argv=', process.argv);
+
+  if (process.argv.length !== 3) {
+    console.log('Error: Please provide the name of a language to remove');
+    process.exit(1);
+  }
+
+  const languageNameToRemove = process.argv[2];
+
+  const languages = JSON.parse(await readFile('data/languages.json', 'utf8'));
+  const languageToRemove = languages.find(
+    (language: Language) => language.name === languageNameToRemove
+  );
+  const filteredLanguages = languages.filter(
+    (language: Language) => language.name !== languageToRemove
+  );
+
+  console.log('languages.length=', languages.length);
+  console.log('filteredLanguages.length=', filteredLanguages.length);
+  console.log('languageToRemove=', languageToRemove);
+};
+
+main();
